874ef7329c3dfeca1b543182e7274e9fdffb00dc
carloseduardoscc <carloseduscc@gmail.com>
2025-10-04 12:48:33 -0300

    refactor(Repo): centralizada repetição de lógica dos repo inMemory em classe abstrata

---
+++ src/main/java/br/com/ciaaerea/repositories/inMemory/InMemoryRepository.java
@@ -0,0 +1,26 @@
+package br.com.ciaaerea.repositories.inMemory;
+
+import br.com.ciaaerea.domain.model.Aeronave;
+import br.com.ciaaerea.repositories.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class InMemoryRepository <T> implements Repository<T> {
+    List<T> list = new ArrayList<>();
+
+    @Override
+    public void add(T T) {
+        list.add(T);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return new ArrayList<>(list);
+    }
+
+    @Override
+    public T findByIndex(int index) {
+        return list.get(index);
+    }
+}
--- src/main/java/br/com/ciaaerea/repositories/inMemory/PassageiroRepository.java
+++ src/main/java/br/com/ciaaerea/repositories/inMemory/PassageiroRepository.java
@@ -1,10 +1,11 @@
 package br.com.ciaaerea.repositories.inMemory;
 
 import br.com.ciaaerea.domain.model.Aeronave;
+import br.com.ciaaerea.domain.model.Passageiro;
 import br.com.ciaaerea.repositories.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AeronaveRepository extends InMemoryRepository<Aeronave> {
+public final class PassageiroRepository extends InMemoryRepository<Passageiro> {
 }
